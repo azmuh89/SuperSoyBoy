6b1e16720fc060275e2723c9bf5f9892cb560907
Humza Khan <humza.khan@msn.com>
2019-11-04 16:18:50 -0500

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text timerText;
+
+    void Awake()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        timerText.text = System.Math.Round((decimal)Time.timeSinceLevelLoad, 2).ToString();
+    }
+}
