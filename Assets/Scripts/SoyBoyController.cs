c272e9deaa19fb96922ed639e8c51affd9004189
Humza Khan <humza.khan@msn.com>
2019-11-04 14:56:41 -0500

    Added jumping to script

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -13,16 +13,52 @@ public class SoyBoyController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
 
+    public bool isJumping;
+    public float jumpSpeed = 8f;
+    private float rayCastLengthCheck = 0.005f;
+    private float width;
+    private float height;
+
+    public float jumpDurationThreshold = 0.25f;
+    private float jumpDuration;
+
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
 
     void Start()
     {
         
+    }
+
+    public bool PlayerIsOnGround()
+    {
+        //perform Raycast directly below character
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(
+            transform.position.x, transform.position.y - height),
+            -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(
+            transform.position.x + (width - 0.2f), transform.position.y - height),
+            -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(
+            transform.position.x - (width - 0.2f), transform.position.y - height),
+            -Vector2.up, rayCastLengthCheck);
+
+        //if any ground check is true, returns true
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     
     void Update()
@@ -40,6 +76,26 @@ public class SoyBoyController : MonoBehaviour
         {
             sr.flipX = true;
         }
+
+        if (input.y >= 1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        if (jumpDuration > jumpDurationThreshold) input.y = 0f;
     }
 
     void FixedUpdate()
@@ -62,5 +118,10 @@ public class SoyBoyController : MonoBehaviour
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         // Velocity is reset on rb so it can stop Super Soy Boy from moving left or right when controls are in neutral state
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 }
