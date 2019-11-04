e383885832f0a624cb198bf8932525a888c9cec2
Humza Khan <humza.khan@msn.com>
2019-11-04 16:16:20 -0500

    Added Goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public AudioClip goalClip;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+
+            if (audioSource != null && goalClip != null)
+            {
+                audioSource.PlayOneShot(goalClip);
+            }
+            GameManager.instance.RestartLevel(0.5f);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
